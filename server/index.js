957486a8eb27c66313ac73f1ae3038e567265fac
braalexdeveloper <braalexdeveloper@gmail.com>
2022-10-06 09:52:24 -0500

    port

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const app = require("./app");
 const { conn } = require("./src/db");
 
-app.set("port", process.env.PORT || 3001);
+/*app.set("port", process.env.PORT || 3001);
 //Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   app.listen(app.get("port"), () => {
     console.log(`Servidor escuchando en el puerto ${app.get("port")}`); // eslint-disable-line no-console
+  });
+});*/
+
+// Syncing all the models at once.
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
